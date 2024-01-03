90b2a64dfbbf269775f4b624f0f74caaadb8dec2
Sneh Desai <sneh1096@Snehs-MBP.lan>
2024-01-02 21:19:49 -0600

    Adding all the files to the Repo

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    // Redirect to a new page (replace 'new-page.html' with the actual page URL)
+    window.location.href = 'yes-response.html';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    window.location.href = 'no-response.html';
+});
